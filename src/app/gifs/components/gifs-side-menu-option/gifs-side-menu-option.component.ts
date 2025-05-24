2edc5514e54ec74efc7d8376701832f9a9d383ca
RUST27 <teranjuarezcristian@gmail.com>
2025-05-24 14:13:47 -0600

    implementaciond de Router Links desde los componentes hijos #72

--- src/app/gifs/components/gifs-side-menu-option/gifs-side-menu-option.component.ts
+++ src/app/gifs/components/gifs-side-menu-option/gifs-side-menu-option.component.ts
@@ -1,9 +1,32 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOption {
+  label: string;
+  subLabel: string;
+  router: string;
+  icon: string;
+}
 
 @Component({
   selector: 'app-gifs-side-menu-option',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './gifs-side-menu-option.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class GifsSideMenuOptionComponent { }
+export class GifsSideMenuOptionComponent {
+
+  menuOptions: MenuOption[] = [{
+    icon: 'fa-solid fa-chart-line',
+    label: 'Trending',
+    subLabel: 'gifs populares',
+    router: '/dashboard/trending'
+  },
+  {
+    icon: 'fa-solid fa-magnifying-glass',
+    label: 'Search',
+    subLabel: 'gifs por nombre',
+    router: '/dashboard/search'
+  }]
+
+}
