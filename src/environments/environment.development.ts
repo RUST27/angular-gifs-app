b29d688e99992d545d0d769fe96429eb551bf5c7
RUST27 <teranjuarezcristian@gmail.com>
2025-05-27 00:03:02 -0600

    se agregaron environments y paths alias #73

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+  production: true,
+  companyNmae: 'Gifs',
+  companyName2: 'App',
+  companySlogan: 'Your GIFs, Your Way',
+  apiUrl: 'https://api.gifsapp.com',
+  apiVersion: 'v1',
+  apiKey: ''
+};
